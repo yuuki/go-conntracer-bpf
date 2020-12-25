de8e47f5695582e61a69e9ddc7c41e6d2f45a90a
yuuki <yuki.tsubo@gmail.com>
2020-12-25 17:36:42 +0900

    Update ubuntu 20.10 for kernel built with BTS

--- provisioning/setup_dev.sh
+++ provisioning/setup_dev.sh
@@ -2,12 +2,31 @@
 
 set -eux
 
-sudo apt-get update
-apt-get -y install --no-install-recommends --fix-missing \
+sudo apt-get -y update
+sudo apt-get -y install --no-install-recommends --fix-missing \
     gpg gpg-agent libz-dev libelf-dev ca-certificates \
     git make curl clang llvm build-essential gcc \
-    linux-headers-$(uname -r) \
-    && apt-get purge --auto-remove && apt-get clean
+    pkg-config \
+    linux-headers-$(uname -r)
+sudo apt-get purge --auto-remove && sudo apt-get clean
+
+# Support BTF
+# sudo apt-get install kernel-package
+
+# Debug Info for libbpf
+# U=http://ddebs.ubuntu.com
+# D=$(lsb_release -cs)
+# cat <<EOF | sudo tee /etc/apt/sources.list.d/ddebs.list
+# deb ${U} ${D} main restricted universe multiverse
+# #deb ${U} ${D}-security main restricted universe multiverse
+# deb ${U} ${D}-updates main restricted universe multiverse
+# deb ${U} ${D}-proposed main restricted universe multiverse
+# EOF
+# wget -O - http://ddebs.ubuntu.com/dbgsym-release-key.asc | \
+# sudo apt-key add -
+# sudo apt-get update -y
+# sudo apt-get install -y linux-image-`uname -r`-dbgsym dwarves
+# sudo apt-get purge --auto-remove && sudo apt-get clean
 
 # Install Go
 GOVER='1.15.5'
