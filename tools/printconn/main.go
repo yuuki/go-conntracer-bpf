df1e3669fa373fb37fe6c044f922b9c8a080baff
yuuki <yuki.tsubo@gmail.com>
2021-01-08 23:15:18 +0900

    printconn: pretty print

--- tools/printconn/main.go
+++ tools/printconn/main.go
@@ -15,13 +15,20 @@ import (
 var interval time.Duration
 
 func init() {
+	log.SetFlags(0)
+
 	flag.DurationVar(&interval, "interval", 3*time.Second, "polling interval (default 3s)")
 	flag.Parse()
 }
 
 func printFlows(flows []*conntracer.Flow) error {
 	for _, flow := range flows {
-		fmt.Printf("%v\n", flow)
+		switch flow.Direction {
+		case conntracer.FlowActive:
+			fmt.Printf("%-25s %-25s %-20d %-10d\n", flow.SAddr, flow.DAddr, flow.LPort, flow.Stat.PID)
+		case conntracer.FlowPassive:
+			fmt.Printf("%-25s %-25s %-20d %-10d\n", flow.DAddr, flow.SAddr, flow.LPort, flow.Stat.PID)
+		}
 	}
 	return nil
 }
@@ -33,11 +40,16 @@ func main() {
 		os.Exit(-1)
 	}
 	defer t.Close()
+
 	t.Start(interval)
 
 	sigch := make(chan os.Signal, 1)
 	signal.Notify(sigch, syscall.SIGTERM, syscall.SIGINT)
 	log.Printf("Waiting interval %s for flows to be collected...\n", interval)
+
+	// print header
+	fmt.Printf("%-25s %-25s %-20s %-10s\n", "LADDR", "RADDR", "RPORT", "PID")
+
 	sig := <-sigch
 	log.Printf("Received %s, Goodbye\n", sig)
 }
