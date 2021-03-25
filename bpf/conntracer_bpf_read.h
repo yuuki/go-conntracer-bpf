3a45153966f0c57eb753937df81811b23019ef20
yuuki <yuki.tsubo@gmail.com>
2021-03-25 11:25:19 +0900

    bpf: add tcp connect and udp tracing code in in-flow-aggr

--- bpf/conntracer_bpf_read.h
+++ bpf/conntracer_bpf_read.h
@@ -56,4 +56,57 @@ static __always_inline void read_flow_for_udp_recv(struct ipv4_flow_key *flow_ke
 	flow_key->l4_proto = IPPROTO_UDP;
 }
 
+static __always_inline 
+void read_flow_tuple_for_udp_send(struct flow_tuple *tuple, 
+	__u8 *direction, __u16 *lport, struct sock *sk, struct flowi4 *flw4)
+{
+	__u16 dport, sport;
+
+	BPF_CORE_READ_INTO(&sport, sk, __sk_common.skc_num);
+	BPF_CORE_READ_INTO(&dport, sk, __sk_common.skc_dport);
+
+	__u8 *sstate = bpf_map_lookup_elem(&udp_port_binding, &sport);
+	if (sstate) {
+		BPF_CORE_READ_INTO(&tuple->saddr, flw4, daddr);
+		BPF_CORE_READ_INTO(&tuple->daddr, flw4, saddr);
+		*direction = FLOW_PASSIVE;
+		*lport = bpf_htons(sport);
+	} else {
+		BPF_CORE_READ_INTO(&tuple->saddr, flw4, saddr);
+		BPF_CORE_READ_INTO(&tuple->daddr, flw4, daddr);
+		*direction = FLOW_ACTIVE;
+		*lport = dport;
+	}
+	tuple->l4_proto = IPPROTO_UDP;
+}
+
+static __always_inline
+void read_flow_tuple_for_udp_recv(struct flow_tuple *tuple, 
+	__u8 *direction, __u16 *lport, struct sock *sk, struct sk_buff *skb)
+{
+	struct udphdr *udphdr = (struct udphdr *)(BPF_CORE_READ(skb, head)
+		+ BPF_CORE_READ(skb,transport_header));
+	struct iphdr *iphdr = (struct iphdr *)(BPF_CORE_READ(skb, head)
+        + BPF_CORE_READ(skb, network_header));
+
+	__u16 sport = BPF_CORE_READ(udphdr, source);
+	__u16 dport = BPF_CORE_READ(udphdr, dest);
+
+	__u16 dport_key = bpf_htons(dport);
+	__u8 *sstate = bpf_map_lookup_elem(&udp_port_binding, &dport_key);
+	if (sstate) {
+		tuple->saddr = BPF_CORE_READ(iphdr, saddr);
+		tuple->daddr = BPF_CORE_READ(iphdr, daddr);
+		*direction = FLOW_PASSIVE;
+		*lport = dport;
+	} else {
+		tuple->saddr = BPF_CORE_READ(iphdr, daddr);
+		tuple->daddr = BPF_CORE_READ(iphdr, saddr);
+		*direction = FLOW_ACTIVE;
+		*lport = sport;
+	}
+
+	tuple->l4_proto = IPPROTO_UDP;
+}
+
 #endif
--- bpf/conntracer_in_flow_aggr.bpf.c
+++ bpf/conntracer_in_flow_aggr.bpf.c
@@ -10,8 +10,169 @@
 
 #include "conntracer.h"
 #include "port_binding.h"
+#include "conntracer_bpf_read.h"
+
+#define AF_INET		2
+#define AF_INET6	10
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, struct flow_tuple);
+	__type(value, struct single_flow);
+	__uint(max_entries, MAX_FLOW_ENTRIES);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+} flows SEC(".maps");
+
+static __always_inline void
+insert_tcp_flows(pid_t pid, struct sock *sk, __u8 direction)
+{
+	struct single_flow flow = {}, *val;
+	struct flow_tuple tuple = {};
+
+	BPF_CORE_READ_INTO(&tuple.saddr, sk, __sk_common.skc_rcv_saddr);
+	BPF_CORE_READ_INTO(&tuple.daddr, sk, __sk_common.skc_daddr);
+	BPF_CORE_READ_INTO(&tuple.sport, sk, __sk_common.skc_num);
+	BPF_CORE_READ_INTO(&tuple.dport, sk, __sk_common.skc_dport);
+	flow.sport = tuple.sport;
+	flow.dport = tuple.dport;
+	switch (direction) {
+		case FLOW_ACTIVE:
+			flow.lport = tuple.dport;
+			break;
+		case FLOW_PASSIVE:
+			flow.lport = tuple.sport;
+			break;
+		default:
+			log_debug("unknown direction:%d pid:%u\n", direction, pid_tgid);
+			return;
+	}
+	tuple.pid = flow.pid = pid;
+	flow.direction = direction;
+	bpf_get_current_comm(flow.task, sizeof(flow.task));
+
+	flow.saddr = tuple.saddr;
+	flow.daddr = tuple.daddr;
+	flow.l4_proto = tuple.l4_proto = IPPROTO_TCP;
+
+	val = bpf_map_lookup_elem(&flows, &tuple);
+	if (val) {
+		return;
+	}
+	bpf_map_update_elem(&flows, &tuple, &flow, BPF_ANY);
+}
+
+static __always_inline void
+insert_udp_flows(struct flow_tuple* tuple, __u8 direction, __u16 lport)
+{
+	struct single_flow flow = {};
+
+	flow.saddr = tuple->saddr;
+	flow.daddr = tuple->daddr;
+	flow.sport = tuple->sport;
+	flow.dport = tuple->dport;
+	flow.direction = direction;
+	flow.l4_proto = tuple->l4_proto;
+	flow.pid = tuple->pid;
+	bpf_get_current_comm(flow.task, sizeof(flow.task));
+
+	bpf_map_update_elem(&flows, tuple, &flow, BPF_ANY);
+}
+
+SEC("kprobe/tcp_v4_connect")
+int BPF_KPROBE(tcp_v4_connect, struct sock *sk)
+{
+	__u64 pid_tgid = bpf_get_current_pid_tgid();
+	__u32 pid = pid_tgid >> 32;
+	__u32 tid = pid_tgid;
+
+	bpf_map_update_elem(&tcp_connect_sockets, &tid, &sk, BPF_ANY);
+
+	log_debug("kprobe/tcp_v4_connect: pid_tgid:%u\n", pid_tgid);
+	return 0;
+}
+
+SEC("kretprobe/tcp_v4_connect")
+int BPF_KRETPROBE(tcp_v4_connect_ret, int ret)
+{
+	__u64 tgid = bpf_get_current_pid_tgid();
+	__u32 pid = tgid >> 32;
+	__u16 dport;
+
+	struct sock** skpp = bpf_map_lookup_elem(&tcp_connect_sockets, &tgid);
+	if (!skpp)
+		return 0;
+
+	if (ret)
+		goto end;
+
+	struct sock* sk = *skpp;
+
+	insert_tcp_flows(pid, sk, FLOW_ACTIVE);
+
+	log_debug("kretprobe/tcp_v4_connect: dport:%u, tid:%u\n", dport, tgid);
+end:
+	bpf_map_delete_elem(&tcp_connect_sockets, &tgid);
+	return 0;
+}
+
+SEC("kretprobe/inet_csk_accept")
+int BPF_KRETPROBE(inet_csk_accept_ret, struct sock *sk)
+{
+	__u64 tgid = bpf_get_current_pid_tgid();
+	__u32 pid = tgid >> 32;
+	__u16 lport = 0;
+
+	if (!sk) {
+		return 0;
+	}
+
+	insert_tcp_flows(pid, sk, FLOW_PASSIVE);
+
+	log_debug("kretprobe/inet_csk_accept: lport:%u,pid_tgid:%u\n", tgid, lport);
+	return 0;
+}
+
+// struct sock with udp_sendmsg may not miss ip addresses on listening socket.
+// Addresses are retrieved from struct flowi4 with ip_make_skb.
+// https://github.com/DataDog/datadog-agent/pull/6307
+SEC("kprobe/ip_make_skb")
+int BPF_KPROBE(ip_make_skb, struct sock *sk, struct flowi4 *flw4) {
+	__u64 pid_tgid = bpf_get_current_pid_tgid();
+	__u32 pid = pid_tgid >> 32;
+	struct flow_tuple tuple = {};
+	__u8 direction;
+	__u16 lport;
+
+	read_flow_tuple_for_udp_send(&tuple, &direction, &lport, sk, flw4);
+	tuple.pid = pid;
+	insert_udp_flows(&tuple, direction, lport);
+
+	log_debug("kprobe/ip_make_skb: sport:%u, dport:%u, tgid:%u\n",
+		tuple.sport, tuple.dport, pid_tgid);
+	return 0;
+}
+
+// struct sock with udp_recvmsg may not miss ip addresses on listening socket.
+// Addresses are retrieved from arguments of skb_consume_udp.
+SEC("kprobe/skb_consume_udp")
+int BPF_KPROBE(skb_consume_udp, struct sock *sk, struct sk_buff *skb) {
+	__u64 tgid = bpf_get_current_pid_tgid();
+	__u32 pid = tgid >> 32;
+	struct flow_tuple tuple = {};
+	__u8 direction;
+	__u16 lport;
+
+	read_flow_tuple_for_udp_recv(&tuple, &direction, &lport, sk, skb);
+	tuple.pid = pid;
+	insert_udp_flows(&tuple, direction, lport);
+
+	log_debug("kprobe/skb_consume_udp: sport:%u, dport:%u, tid:%u\n",
+		sport, dport, tgid);
+    return 0;
+}
+
 
 // for tracking UDP listening state
 SEC("tracepoint/syscalls/sys_enter_socket")
